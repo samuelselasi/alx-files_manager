3c4de3cc2bf8fec3fe2c2fab141aa94db7be77cc
samuelselasi <samelsekasi@gamil.com>
2023-12-01 07:42:44 -0800

    Add module that connects to redis client

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+// module that connects to Redis client
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getClient = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+  }
+
+  isAlive() {
+    return !this.client.connected;
+  }
+
+  async get(key) {
+    const value = await this.getClient(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+const redisClient = new RedisClient();
+export default redisClient;
