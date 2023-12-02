fb2077cf62c96ff6a5f4f220247ed0d203c78bd9
samuelselasi <samelsekasi@gamil.com>
2023-12-02 07:29:53 -0800

    Fix new server

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,6 +7,7 @@ const userQueue = new Queue('email sending');
 
 class UsersController {
   static async postNew(req, res) {
+    // console.log('Request Body:', req.body);
     const { email } = req.body;
     const { password } = req.body;
 
@@ -19,14 +20,17 @@ class UsersController {
       return;
     }
 
-    const User = await (await dbClient.usersCollection()).findOne({ email });
+    // const User = await (await dbClient.usersCollection()).findOne({ email });
+    const usersCollection = await dbClient.client.db().collection('users');
+    const User = await usersCollection.findOne({ email });
 
     if (User) {
       res.status(400).json({ error: 'Already exist' });
       return;
     }
-    const insertData = await (await dbClient.usersCollection())
-      .insertOne({ email, password: sha1(password) });
+    const insertData = await usersCollection.insertOne(
+      { email, password: sha1(password) },
+    );
     const userId = insertData.insertedId.toString();
 
     userQueue.add({ userId });
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ import router from './routes';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(router);
 app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
