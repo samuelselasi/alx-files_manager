3700966f38dba71c02b54cab32fbe6c6b675c289
SKGakiria <swgakiria@gmail.com>
2023-12-02 02:58:07 -0800

    Updated the index route with POST

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 // Contains all endpoints for the API
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 export default router;
